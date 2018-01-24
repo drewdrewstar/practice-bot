b0bea9f24653f561288232b51a7598653793d6cc
Drew <pikamarisa64@gmail.com>
2018-01-24 13:25:22 -0500

    Update pseudoAuto.java
    Basic idea of the code, feel free to criticize me

--- pseudoAuto.java
+++ pseudoAuto.java
@@ -1,1 +1,41 @@
 //pseudo code for planning autonomous
+//insert basic robotDrive junk
+//start
+
+String gameData;
+		gameData = DriverStation.getInstance().getGameSpecificMessage();
+int startingPos
+
+//Switch Determination
+//Manually input the startingPos somehow??? idk if thats even possible
+
+if (gameData.getAt(0) == 'L'){
+  switch (startingPos){
+    case 1:
+      //code to drive to switch from this position
+      break;
+    
+    case 2:
+      //code to drive to switch from this position
+      break;
+    
+    case 3:
+      //code to drive to switch from this position
+      break;
+    }
+}
+  else {                                            //If the switch position is not L, the else gives the directions for position R  
+    switch startingPos() {                          //switch to determine distance needed to drive to the switch
+      case 1:
+        //code to drive to switch from this position
+        break;
+      
+      case 2:
+        //code to drive to switch from this position
+      break;
+      
+      case 3:
+        //code to drive to switch from this position
+      break;
+      }
+  }
